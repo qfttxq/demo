561ddf092de234a9925b32e972e9b3df6b250207
zhangzengrong <qfttxq@163.com>
2018-09-20 16:57:02 +0800

    commit 1

--- zipkin-service/src/main/java/com/example/demo/ZipkinServiceApplication.java
+++ zipkin-service/src/main/java/com/example/demo/ZipkinServiceApplication.java
@@ -2,13 +2,13 @@ package com.example.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.config.server.EnableConfigServer;
+import zipkin.server.EnableZipkinServer;
 
 @SpringBootApplication
-@EnableConfigServer
-public class ConfigServiceApplication {
+@EnableZipkinServer
+public class ZipkinServiceApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(ConfigServiceApplication.class, args);
+		SpringApplication.run(ZipkinServiceApplication.class, args);
 	}
 }
